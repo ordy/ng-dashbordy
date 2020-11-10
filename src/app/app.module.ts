b9050fbd8e3687a1c668dc1974c25af1cd96c80c
Sandro T <ordysan@gmail.com>
2020-11-10 22:48:00 +0100

    app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from '../routing/app-routing.module';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
+import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    SidebarComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, ButtonModule, CardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
