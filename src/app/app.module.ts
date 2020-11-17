dac370d63cc41abbd5f878712162eff0b6d6df44
Sandro T <ordysan@gmail.com>
2020-11-17 17:03:51 +0100

    including firestore packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
@@ -12,6 +16,7 @@ import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -23,7 +28,15 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     DashboardComponent,
     SidebarComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, ButtonModule, CardModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ButtonModule,
+    CardModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
