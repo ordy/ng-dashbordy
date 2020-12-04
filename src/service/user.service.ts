09650eb763c89976f9281594ec334e83b46284dd
Sandro T <ordysan@gmail.com>
2020-12-04 15:11:10 +0100

    created firestore functions, rng password gen

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -3,6 +3,7 @@ import { User } from '../shared/user';
 import {
   AngularFirestore,
   AngularFirestoreCollection,
+  DocumentData,
 } from '@angular/fire/firestore';
 import { AuthService } from './auth.service';
 
@@ -12,25 +13,26 @@ import { AuthService } from './auth.service';
 export class UserService {
   public usersList: User[];
   public usersDB: AngularFirestoreCollection;
+  public docRef;
 
   constructor(public db: AngularFirestore, public authS: AuthService) {
-    console.log('in user service');
+    this.usersDB = this.db.collection('users');
   }
 
   async fetchUsers(): Promise<User[]> {
     if (this.usersList == null) {
-      this.usersDB = this.db.collection('users');
       this.usersList = [];
       await this.usersDB.get().forEach((el) => {
         el.forEach((x) => {
           const user: User = {
-            uid: 'toDefine',
+            uid: x.get('uid'),
             firstname: x.get('firstname'),
             surname: x.get('surname'),
             email: x.get('email'),
             city: x.get('city'),
+            region: x.get('region'),
             phone: x.get('phone'),
-            lastLogged: x.get('lastLogged'),
+            lastLogged: x.get('lastLogged').toDate(),
             isAdmin: x.get('isAdmin'),
           };
           this.usersList.push(user);
@@ -40,7 +42,53 @@ export class UserService {
     return this.usersList;
   }
 
-  addUser() {}
-  deleteUser() {}
-  updateUser() {}
+  userRef(collection: string, field: string, value: string): DocumentData {
+    return this.db.firestore.collection(collection).where(field, '==', value);
+  }
+
+  addUser(newUser: User): void {
+    //newUser.uid = this.authS.signUp(newUser.email, this.passwordGen());
+    this.usersList.push(newUser);
+  }
+
+  deleteUser(userEmail: string): void {
+    this.userRef('users', 'email', userEmail)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          doc.ref.delete();
+        });
+      });
+  }
+
+  updateUser(user: User): void {
+    user.lastLogged = new Date();
+    this.userRef('users', 'email', user.email)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          doc.ref.set(user);
+        });
+      });
+  }
+
+  // Return a random password between 16 and 25 characters
+  passwordGen = (): string => {
+    let pw = '';
+    const allChars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-!@#$%&*ˆ+=_]';
+    const specials = '-!@#$%&*ˆ+=_]';
+    const numbers = '0123456789';
+    pw = pw.concat(
+      specials.charAt(Math.floor(Math.random() * specials.length))
+    );
+    pw = pw.concat(numbers.charAt(Math.floor(Math.random() * 10)));
+    const charsLength = allChars.length;
+    // returns a random number from 14 to 23
+    const size = Math.floor(Math.random() * 9) + 14;
+    for (let i = 0; i < size; i++) {
+      pw = pw.concat(allChars.charAt(Math.floor(Math.random() * charsLength)));
+    }
+    return pw;
+  };
 }
