47d0da1a53d8666fa5c9f01a34c0c50b2ab730e9
Sandro T <ordysan@gmail.com>
2020-12-08 15:38:47 +0100

    refactoring of user saving function

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -14,6 +14,7 @@ export class UsersComponent implements OnInit {
   public selectedUser: User;
   public dialogUser: User;
   public showDialog: boolean;
+  public submitted: boolean;
 
   constructor(
     public userS: UserService,
@@ -30,13 +31,10 @@ export class UsersComponent implements OnInit {
     this.usersTable = await this.userS.fetchUsers();
   }
 
-  addUser() {
-    this.messageService.add({
-      severity: 'success',
-      summary: 'New user',
-      detail: 'Successfully added!',
-    });
-    //this.userS.updateUser(this.selectedUser);
+  addUser(): void {
+    this.submitted = false;
+    this.dialogUser = {};
+    this.showDialog = true;
   }
 
   deleteUser(): void {
@@ -53,39 +51,57 @@ export class UsersComponent implements OnInit {
         this.usersTable = this.usersTable.filter(
           (res) => res.email !== this.selectedUser.email
         );
-        this.userS.deleteUser(this.selectedUser.email);
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Deleted',
-          detail: 'The user has been removed!',
-          life: 3000,
-        });
+        this.userS.deleteUser(this.selectedUser.uid);
+        this.notifyMsg('success', 'Deleted', 'The user has been removed!');
       },
     });
   }
 
   // Working on a copy to avoid data binding on input
   editUser(): void {
-    this.showDialog = true;
     this.dialogUser = { ...this.selectedUser };
+    this.showDialog = true;
   }
 
   saveUser(): void {
-    const currentUser = this.usersTable.findIndex(
-      (usr) => usr.email === this.dialogUser.email
-    );
-    this.usersTable[currentUser] = { ...this.dialogUser };
-    this.selectedUser = { ...this.dialogUser };
-    console.log(this.selectedUser);
-    this.userS.updateUser(this.selectedUser);
+    this.submitted = true;
+    if (this.dialogUser.uid) {
+      const currentUser = this.usersTable.findIndex(
+        (usr) => usr.email === this.dialogUser.email
+      );
+      this.usersTable[currentUser] = { ...this.dialogUser };
+      this.selectedUser = { ...this.dialogUser };
+      this.userS.updateUser(this.selectedUser);
+      this.showDialog = false;
+      this.notifyMsg('success', 'Edit user', 'Successfully edited!');
+    } else {
+      this.userS.addUser(this.dialogUser).then((res) => {
+        this.dialogUser.uid = res;
+        this.usersTable.push(this.dialogUser);
+      });
+      this.showDialog = false;
+      this.notifyMsg('success', 'New user', 'Successfully added!');
+    }
+  }
+
+  notifyMsg(type: string, title: string, message: string): void {
+    this.messageService.add({
+      severity: type,
+      summary: title,
+      detail: message,
+    });
+  }
+
+  hideDialog(): void {
+    this.submitted = false;
     this.showDialog = false;
   }
 
-  hideDialog() {
-    this.showDialog = false;
-  }
-
-  toDate(date: Date): string {
-    return date.toLocaleDateString();
+  logUser(): void {
+    this.usersTable.forEach((el) => {
+      for (const [key, value] of Object.entries(el)) {
+        console.log(key, value);
+      }
+    });
   }
 }
