8e2b8e8027cc5f69b586b00fbc8353bcef6c7fda
Sandro T <ordysan@gmail.com>
2020-12-15 14:59:13 +0100

    created login status and logout button

--- src/shared/header/header.component.ts
+++ src/shared/header/header.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../service/auth.service';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  public userName: string;
+  public currentUser: Observable<string>;
+  public isLogged: Observable<boolean>;
+  public items: MenuItem[];
 
-  constructor() { }
+  constructor(private authS: AuthService) {}
 
   ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Log out',
+        icon: 'pi pi-fw pi-power-off',
+        command: () => {
+          this.logout();
+        },
+      },
+    ];
+    this.isLogged = this.authS.isLoggedIn;
+    this.currentUser = this.authS.currentUserID;
   }
 
+  logout(): void {
+    this.authS.signOut();
+  }
 }
