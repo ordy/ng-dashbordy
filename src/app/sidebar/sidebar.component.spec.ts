d71c603c76809e16328bba412f0acd92b663f538
Sandro T <ordysan@gmail.com>
2020-12-29 01:17:31 +0100

    created unit tests on sidebar and users components

--- src/app/sidebar/sidebar.component.spec.ts
+++ src/app/sidebar/sidebar.component.spec.ts
@@ -1,25 +1,34 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { SidebarComponent } from './sidebar.component';
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
+  let de: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SidebarComponent ]
-    })
-    .compileComponents();
+      declarations: [SidebarComponent],
+      imports: [RouterModule.forRoot([])],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a UL tag ', () => {
+    expect(de.query(By.css('ul')).nativeElement.innerText).toBeTruthy();
   });
 });
--- src/app/users/users.component.spec.ts
+++ src/app/users/users.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { CardModule } from 'primeng/card';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { DialogModule } from 'primeng/dialog';
@@ -6,6 +11,7 @@ import { TableModule } from 'primeng/table';
 import { ToastModule } from 'primeng/toast';
 import { ToolbarModule } from 'primeng/toolbar';
 import { UserService } from 'src/service/user.service';
+import { User } from 'src/shared/user';
 
 import { UsersComponent } from './users.component';
 
@@ -34,9 +40,118 @@ describe('UsersComponent', () => {
     fixture.detectChanges();
   });
 
+  beforeEach(fakeAsync(() => {
+    component.getUsers();
+    tick();
+  }));
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should open the dialog and setting the global variables', () => {
+    component.addUser();
+    expect(component.submitted).toBeFalse();
+    expect(component.showDialog).toBeTrue();
+    expect(component.dialogUser).toEqual({});
+  });
+
+  it('should hide the dialog', () => {
+    component.hideDialog();
+    expect(component.submitted).toBeFalse();
+    expect(component.showDialog).toBeFalse();
+  });
+
+  it('should return admin', () => {
+    const role = true;
+    expect(component.roles(role)).toEqual('admin');
+  });
+
+  it('should have valid data', () => {
+    const [firstname, surname] = ['name1', 'name2'];
+    let email = 'email';
+    expect(component.isValid(firstname, surname, email)).toBeTrue();
+    email = '';
+    expect(component.isValid(firstname, surname, email)).toBeFalse();
+    expect(component.isValid(firstname, surname)).toBeFalse();
+  });
+
+  it('should be able to fetch users from the service', fakeAsync(() => {
+    component.getUsers();
+    tick();
+    expect(component.usersTable.length).toBeGreaterThan(0);
+  }));
+
+  it('should edit an existing user', () => {
+    const newUser: User = {
+      uid: 'uid1',
+      firstname: 'fn1-updated',
+      surname: 'sn1',
+      email: 'mail@1.com',
+      city: 'city1',
+      region: 'region1',
+      phone: '(111) 111 - 1111',
+      lastLogged: new Date(),
+      isAdmin: true,
+    };
+    component.dialogUser = { ...newUser };
+    component.saveUser();
+    expect(component.usersTable[0].firstname).toEqual('fn1-updated');
+  });
+
+  it('should save a new user', fakeAsync(() => {
+    const sizeOfTable = component.usersTable.length;
+    const newUser: User = {
+      firstname: 'fn3',
+      surname: 'sn3',
+      email: 'mail@3.com',
+      city: 'city3',
+      region: 'region3',
+      phone: '(333) 333 - 3333',
+      lastLogged: new Date(),
+      isAdmin: true,
+    };
+    component.dialogUser = { ...newUser };
+    component.saveUser();
+    tick();
+    expect(component.usersTable.length).toEqual(sizeOfTable + 1);
+  }));
 });
 
-class UserServiceStub {}
+class UserServiceStub {
+  public usersList: User[];
+
+  async fetchUsers(): Promise<User[]> {
+    this.usersList = [
+      {
+        uid: 'uid1',
+        firstname: 'fn1',
+        surname: 'sn1',
+        email: 'mail@1.com',
+        city: 'city1',
+        region: 'region1',
+        phone: '(111) 111 - 1111',
+        lastLogged: new Date(),
+        isAdmin: true,
+      },
+      {
+        uid: 'uid2',
+        firstname: 'fn2',
+        surname: 'sn2',
+        email: 'mail@2.com',
+        city: 'city2',
+        region: 'region2',
+        phone: '(222) 222 - 2222',
+        lastLogged: new Date(),
+        isAdmin: true,
+      },
+    ];
+    return this.usersList;
+  }
+
+  updateUser(): void {}
+  async addUser(newUser: User): Promise<void> {
+    newUser.uid = 'uid3';
+    this.usersList.push(newUser);
+  }
+}
