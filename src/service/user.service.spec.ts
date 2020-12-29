0b5dd1be7bf8d9d525019a5a207adcdebaefd3bf
Sandro T <ordysan@gmail.com>
2020-12-29 01:13:41 +0100

    stubbed User Service

--- src/service/user.service.spec.ts
+++ src/service/user.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AuthService } from './auth.service';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
@@ -7,15 +8,26 @@ describe('UserService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [{ provide: UserService, useClass: UserServiceStub }],
+      providers: [
+        { provide: AngularFirestore, useClass: AngularFirestoreStub },
+        { provide: AuthService, useClass: AuthServiceStub },
+      ],
     });
     service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
-    service = TestBed.inject(UserService);
     expect(service).toBeTruthy();
+  });
+
+  it('should return a password between 16 and 25 characters', () => {
+    const pass = service.passwordGen();
+    expect(pass.length).toBeGreaterThanOrEqual(16);
+    expect(pass.length).toBeLessThanOrEqual(25);
   });
 });
 
-class UserServiceStub {}
+class AuthServiceStub {}
+class AngularFirestoreStub {
+  collection(): void {}
+}
