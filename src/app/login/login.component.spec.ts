dedaec23ab8945876cad06b83d964e1307c24015
Sandro T <ordysan@gmail.com>
2020-12-29 01:38:59 +0100

    fixed login unit tests

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,16 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  waitForAsync,
+} from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { AuthService } from '../../service/auth.service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { InputTextModule } from 'primeng/inputtext';
+import { of } from 'rxjs';
+import { CardModule } from 'primeng/card';
+import { PasswordModule } from 'primeng/password';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+    TestBed.configureTestingModule({
+      declarations: [LoginComponent],
+      imports: [FormsModule, InputTextModule, CardModule, PasswordModule],
+      providers: [{ provide: AuthService, useClass: AuthServiceStub }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +36,16 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get the loading state', () => {
+    component.loading.subscribe((x) => {
+      expect(x).toBeFalse();
+    });
+  });
 });
+
+class AuthServiceStub {
+  get isLoading(): Observable<boolean> {
+    return of(false);
+  }
+}
