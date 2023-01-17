c723618a06e18602c5129d2273af2385495763d8
Sandro T <ordysan@gmail.com>
2023-01-17 21:08:59 +0100

    testing env variables

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/service/auth.service';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +10,6 @@ import { environment } from 'src/environments/environment';
 })
 export class LoginComponent implements OnInit {
   public loading: Observable<boolean>;
-  public env = environment.firebaseConfig;
 
   constructor(private authS: AuthService) {
     this.loading = this.authS.isLoading;
